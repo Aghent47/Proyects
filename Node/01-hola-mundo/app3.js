9d64a24bc93eda8db0ea579ef0d2c169d564c55e
Neider Hernández <neiderkk@gmail.com>
2021-04-11 00:16:47 -0500

    first commit

---
+++ Node/01-hola-mundo/app3.js
@@ -0,0 +1,15 @@
+console.log('inicio del programa');
+
+setTimeout(function() {
+    console.log('primer Timeout');
+}, 3000);
+
+setTimeout(function() {
+    console.log('Segundo Timeout');
+}, 0);
+
+setTimeout(function() {
+    console.log('tercer Timeout');
+}, 0);
+
+console.log('fin del programa');
